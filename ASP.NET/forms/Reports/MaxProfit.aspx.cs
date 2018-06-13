01c0daea6eb461f5e33230658731c80fdfeba37b
Alexander <sasha@ALEX>
2018-06-13 21:35:53 +0500

    последняя модификация

--- ASP.NET/forms/Reports/MaxProfit.aspx.cs
+++ ASP.NET/forms/Reports/MaxProfit.aspx.cs
@@ -13,7 +13,12 @@ namespace IIS.АСУ_Кондитерская.forms.Reports
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            MSSQLDataService ds = new MSSQLDataService();
+            ds.CustomizationString = "SERVER=ALEX;Trusted_connection=yes;DATABASE=dbconf;";
+            var res = ds.ExecuteNonQuery(@"select строка.Продукт_m0, sum(строка.Количество*Продукт.Цена) Прибыль
+from СтрокаЗаказа строка left join Продукт on строка.Продукт_m0 = Продукт.primaryKey
+group by строка.Продукт_m0
+order by 2");
         }
     }
 }
